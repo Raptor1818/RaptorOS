cb54b4f99f155276626264bfe36b4eb7ca2be242
Raptor <francyoppureno@gmail.com>
2024-07-13 23:40:18 +0200

    added close window button

--- src/components/dev/PlaceholderWindow.tsx
+++ src/components/dev/PlaceholderWindow.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from 'react'
+import React, { ReactNode, useEffect, useState } from 'react'
 import Draggable from 'react-draggable'
 
 interface Props {
@@ -10,15 +10,21 @@ const PlaceholderWindow = (props: Props) => {
   const [hideWindow, setHideWindow] = useState(false);
 
   const closeWindow = () => {
-    setHideWindow(!hideWindow)
+    setHideWindow(true)
   }
 
-  return (
+  useEffect(() => {
+  }, [hideWindow]);
+
+  if (hideWindow) 
+    return null
+  else 
+    return (
     <Draggable handle='#handler'>
       <div className='h-64 w-96 border flex flex-col justify-between absolute'>
         <div id='handler' className='w-full bg-green-500 p-1 flex flex-row justify-between items-center hover:cursor-move'>
           <h2 className=' select-none w-fit'>{props.windowHeader}</h2>
-          <button className='border cursor-pointer z-10 w-fit px-2 bg-red-500' >X</button>
+          <button className='border cursor-pointer z-10 w-fit px-2 bg-red-500' onClick={closeWindow}>X</button>
         </div>
         <div className='h-full w-full bg-purple-500'>
           <p></p>
