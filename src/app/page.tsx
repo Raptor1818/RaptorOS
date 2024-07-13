409035274363b10a8e5a1c03e17d6976bb28c10b
Raptor <francyoppureno@gmail.com>
2024-07-13 23:29:04 +0200

    Testing windows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+'use client'
+
+import PlaceholderWindow from "@/components/dev/PlaceholderWindow";
+
 export default function Home() {
   return (
-    <main>
-      
+    <main className="min-h-screen w-screen border-red-500 border-2">
+      <PlaceholderWindow windowHeader="bel">p</PlaceholderWindow>
+      <PlaceholderWindow windowHeader="come xe">sdsdds</PlaceholderWindow>
+      <PlaceholderWindow windowHeader="lllllllllllllllll">sds</PlaceholderWindow>
     </main>
   );
 }
---
+++ src/components/dev/PlaceholderWindow.tsx
@@ -0,0 +1,32 @@
+import React, { ReactNode, useState } from 'react'
+import Draggable from 'react-draggable'
+
+interface Props {
+  windowHeader: string;
+  children: ReactNode;
+}
+
+const PlaceholderWindow = (props: Props) => {
+  const [hideWindow, setHideWindow] = useState(false);
+
+  const closeWindow = () => {
+    setHideWindow(!hideWindow)
+  }
+
+  return (
+    <Draggable handle='#handler'>
+      <div className='h-64 w-96 border flex flex-col justify-between absolute'>
+        <div id='handler' className='w-full bg-green-500 p-1 flex flex-row justify-between items-center hover:cursor-move'>
+          <h2 className=' select-none w-fit'>{props.windowHeader}</h2>
+          <button className='border cursor-pointer z-10 w-fit px-2 bg-red-500' >X</button>
+        </div>
+        <div className='h-full w-full bg-purple-500'>
+          <p></p>
+          {props.children}
+        </div>
+      </div>
+    </Draggable>
+  )
+}
+
+export default PlaceholderWindow
