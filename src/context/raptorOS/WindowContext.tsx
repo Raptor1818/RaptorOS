2cab92a9abcc141675144adb2a11aacb5e34fbb1
Raptor <francyoppureno@gmail.com>
2024-08-04 23:13:16 +0200

    Preventing the duplication of windows

--- src/context/raptorOS/WindowContext.tsx
+++ src/context/raptorOS/WindowContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode, useContext, useState } from "react";
+import React, { createContext, useContext, useState, ReactNode } from 'react';
 
 interface Window {
   id: string;
@@ -7,49 +7,49 @@ interface Window {
 }
 
 interface WindowContextType {
-  addWindow: (id: string, title: string, content: ReactNode) => void;
   windows: Window[];
+  zIndexList: string[];
+  addWindow: (id: string, title: string, content: ReactNode) => void;
   closeWindow: (id: string) => void;
   bringToFront: (id: string) => void;
-  zIndexList: string[];
 }
 
 const WindowContext = createContext<WindowContextType | undefined>(undefined);
 
-export const WindowProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+export const useWindowContext = () => {
+  const context = useContext(WindowContext);
+  if (!context) {
+    throw new Error('useWindowContext must be used within a WindowProvider');
+  }
+  return context;
+};
+
+export const WindowProvider = ({ children }: { children: ReactNode }) => {
   const [windows, setWindows] = useState<Window[]>([]);
   const [zIndexList, setZIndexList] = useState<string[]>([]);
 
   const addWindow = (id: string, title: string, content: ReactNode) => {
-    const newWindow = {
-      id,
-      title,
-      content,
-    };
-    setWindows([...windows, newWindow]);
-    setZIndexList([...zIndexList, newWindow.id]);
+    const existingWindow = windows.find(window => window.id === id);
+    if (existingWindow) {
+      bringToFront(id);
+    } else {
+      setWindows([...windows, { id, title, content }]);
+      setZIndexList([...zIndexList, id]);
+    }
   };
 
   const closeWindow = (id: string) => {
     setWindows(windows.filter(window => window.id !== id));
-    setZIndexList(zIndexList.filter(windowId => windowId !== id));
+    setZIndexList(zIndexList.filter(zId => zId !== id));
   };
 
   const bringToFront = (id: string) => {
-    setZIndexList([...zIndexList.filter(windowId => windowId !== id), id]);
+    setZIndexList([...zIndexList.filter(zId => zId !== id), id]);
   };
 
   return (
-    <WindowContext.Provider value={{ addWindow, windows, closeWindow, bringToFront, zIndexList }}>
+    <WindowContext.Provider value={{ windows, addWindow, closeWindow, bringToFront, zIndexList }}>
       {children}
     </WindowContext.Provider>
   );
-};
-
-export const useWindowContext = () => {
-  const context = useContext(WindowContext);
-  if (!context) {
-    throw new Error("useWindowContext must be used within a WindowProvider");
-  }
-  return context;
 };
