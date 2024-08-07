83e5eade027a290310b3125ea24a80a379ea81a3
Raptor <francyoppureno@gmail.com>
2024-08-07 12:34:31 +0200

    Added two fonts
    - Inter (default)
    - Noto Sans
    - Noto Sans Mono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,37 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Noto_Sans, Noto_Sans_Mono } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ 
+  subsets: ["latin"],
+  variable: '--font-inter'
+  });
+const notoSans = Noto_Sans({ 
+  subsets: ["latin"],
+  variable: '--font-noto-sans'
+  });
+const notoSansMono = Noto_Sans_Mono({
+  weight: ['400'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-noto-sans-mono'
+});
 
 export const metadata: Metadata = {
-  title: "RaptorHub",
+  title: "RaptorOS",
   description: "Raptor's personal website, now in Next.js",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${notoSansMono.variable} ${notoSans.variable} ${inter.variable} `}>
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        inter: ['var(--font-inter)'],
+        notoSans: ['var(--font-noto-sans)'],
+        notoMono: ['var(--font-noto-sans-mono)'],
+      },
     },
   },
   plugins: [],
