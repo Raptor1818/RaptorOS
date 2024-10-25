1fbde6bc5d563c1812e65bcc7dd1a150fce28006
Raptor <francyoppureno@gmail.com>
2024-10-25 20:31:19 +0200

    Implemented startup app with search parameter
    Using the router to look for parameters and open the app with the corresponding label, redirecting to '/' afterwards

--- src/context/WindowProvider/window-provider.tsx
+++ src/context/WindowProvider/window-provider.tsx
@@ -1,5 +1,7 @@
 'use client';
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { appList } from '@/lib/lists/app-list';
 
 export interface AppWindowType {
   id: string;
@@ -18,52 +20,108 @@ interface WindowProviderType {
   openWindow: (appWindow: AppWindowType) => void;
   closeWindow: (id: string) => void;
   bringToFront: (id: string) => void;
+  getWindowById: (id: string) => AppWindowType | undefined;
+  getAppById: (id: string) => AppWindowType | undefined;
 }
 
-const WindowContext = createContext<WindowProviderType | undefined>(undefined)
+const WindowContext = createContext<WindowProviderType | undefined>(undefined);
+
+const QUERY_STRING_NAME: string = 'app';
 
 const WindowProvider = ({ children }: { children: React.ReactNode }) => {
   const [windows, setWindows] = useState<AppWindowType[]>([]) // List of all open windows
   const [zIndexList, setZIndexList] = useState<string[]>([]); // zIndex list for correct window layer ordering
   const [focusedWindowId, setFocusedWindowId] = useState<string | null>(null); // What window is in focus right now
 
-  // Add window object to array
-  const openWindow = (appWindow: AppWindowType) => {
-    setWindows([...windows, appWindow])
-    bringToFront(appWindow.id)
-  }
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-  // Removes window object from array
+  // Opens a window and brings it to the front
+  const openWindow = (appWindow: AppWindowType) => {
+    if (!zIndexList.includes(appWindow.id)) {
+      setWindows(prevWindows => [...prevWindows, appWindow]);
+    }
+    bringToFront(appWindow.id);
+  };
+
+  // Closes the window by ID, removes it from both arrays
   const closeWindow = (id: string) => {
-    setWindows(windows.filter(window => window.id !== id))
-  }
+    setWindows(prevWindows => prevWindows.filter(window => window.id !== id));
+    setZIndexList(prevZIndexList => prevZIndexList.filter(zId => zId !== id));
+    if (focusedWindowId === id) setFocusedWindowId(null); // Once closed, sets focused window to null
+  };
 
   // Bring window to the front of the zIndex array
   const bringToFront = (id: string) => {
-    setZIndexList([...zIndexList.filter(zId => zId !== id), id]); // Re-add to place at the end
-    setFocusedWindowId(id); // Set focused window
+    setZIndexList(prevZIndexList => [...prevZIndexList.filter(zId => zId !== id), id]);
+    setFocusedWindowId(id);
   };
 
+  const getWindowById = (id: string) => {
+    return windows.find(window => window.id === id);
+  };
+
+  const getAppById = (id: string) => {
+    return appList.find(app => app.id === id);
+  };
+
+  const getAppByLabel = (label: string) => {
+    return appList.find(app => app.label === label);
+  };
+
+  // Effect to open the app based on URL query when the component mounts
+  // Unfortunately only works on the first render, as it reloads the page editing it
+  // TODO: Add local storage windows to persist between reloads
+  useEffect(() => {
+    const appParameter = searchParams.get(QUERY_STRING_NAME);
+    if (appParameter) {
+      const appInQuestion = getAppByLabel(appParameter);
+      if (appInQuestion) {
+        openWindow(appInQuestion);
+        router.push('/');
+      }
+    }
+  }, [searchParams]);
+
+  // // Effect to update the URL query when the focused window changes
+  // useEffect(() => {
+  //   if (focusedWindowId) {
+  //     const appInQuestion = getWindowById(focusedWindowId);
+  //     if (appInQuestion) {
+  //       // Updates the URL with the window ID as a query parameter
+  //       const routeString = appInQuestion.label.replace(/\s+/g, '-');
+  //       router.push(`?${QUERY_STRING_NAME}=${routeString}`);
+  //     }
+  //   } else {
+  //     router.push('/');
+  //   }
+  // }, [focusedWindowId])
+
   return (
-    <WindowContext.Provider value={{
-      windows,
-      zIndexList,
-      focusedWindowId,
-      openWindow,
-      closeWindow,
-      bringToFront
-    }}>
+    <WindowContext.Provider
+      value={{
+        windows,
+        zIndexList,
+        focusedWindowId,
+        openWindow,
+        closeWindow,
+        bringToFront,
+        getWindowById,
+        getAppById
+      }}
+    >
       {children}
     </WindowContext.Provider>
-  )
-}
+  );
+};
 
+// Hook to access window context
 export function useWindowContext() {
-  const context = useContext(WindowContext)
+  const context = useContext(WindowContext);
   if (!context) {
-    throw new Error('useWindowContext must be used within a WindowProvider')
+    throw new Error('useWindowContext must be used within a WindowProvider');
   }
-  return context
+  return context;
 }
 
-export default WindowProvider
+export default WindowProvider;
