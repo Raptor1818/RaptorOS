38189e54c31d4abed35d98df4f4015b61fc4310c
Raptor <francyoppureno@gmail.com>
2024-10-27 16:57:59 +0100

    Fixed window is undefined error
    imported Desktop as dynamic with ssr disabled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Desktop from "@/components/raptor-os/system/Desktop";
+'use client';
+import dynamic from 'next/dynamic';
+
+const DynamicDesktop = dynamic(() => import('@/components/raptor-os/system/Desktop'), {
+  ssr: false,
+});
 
 export default function HomePage() {
   return (
     <main className="w-screen h-screen flex flex-col" suppressHydrationWarning>
-      <Desktop />
+      <DynamicDesktop />
     </main>
   );
 }
